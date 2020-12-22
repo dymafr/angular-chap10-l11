cfc2ea1b2a2b3865ac8eba59e4a6a8a2fcda2992
dymafr <erwan1309@hotmail.com>
2020-12-22 12:09:49 +0100

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { Routes } from "@angular/router";
+import { HomepageComponent } from "./homepage/homepage.component";
+import { UserComponent } from "./user/user.component";
+import { UsersComponent } from "./users/users.component";
+
+export const APP_ROUTES: Routes = [
+  { path: "", component: HomepageComponent },
+  {
+    path: "users",
+    component: UsersComponent,
+    children: [{ path: ":id", component: UserComponent }]
+  },
+  { path: "**", redirectTo: "" }
+];
+@NgModule({
+  imports: [RouterModule.forRoot(APP_ROUTES)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}

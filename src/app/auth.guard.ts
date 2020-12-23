18f3cc84b58f95f301cc9e3dcfd15e5d188b1598
dymafr <erwan1309@hotmail.com>
2020-12-23 21:24:43 +0100

    1 file modified

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -2,16 +2,24 @@ import { Injectable } from "@angular/core";
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  CanActivateChild,
   RouterStateSnapshot
 } from "@angular/router";
 
 @Injectable({ providedIn: "root" })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean {
     console.log(route, state);
+    return true;
+  }
+
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
     return true;
   }
 }
